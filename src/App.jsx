563d25139d6b36db79823f1d63b40d253c8d7cab
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-09-23 21:16:01 +0600

    store value in state onChange eventHandler and error handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [error, setError] = useState('');
+
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
-    console.log(e)
-    console.log(e.target.name.value)
-    console.log(e.target.email.value)
+    // console.log(e)
+    // console.log(e.target.name.value)
+    // console.log(e.target.email.value)
+
+    if (name === 'arafat' && email === 'arafat@gmail.com') {
+      setError('Match');
+      console.log(name, email)
+
+    } else {
+      setError('No Match');
+    }
   }
 
   return (
     <>
       <h4>Form Submit</h4>
       <form onSubmit={handleFormSubmit}>
-        <input type="text" name="name" id="" /><br />
-        <input type="email" name="email" id="" /><br />
+        <input onChange={handleName} type="text" name="name" id="" /><br />
+        <input onChange={handleEmail} type="email" name="email" id="" /><br />
         <input type="submit" value="submit" />
       </form>
+      {
+        // error && <p>{error}</p>
+
+        error === 'Match' ? <p style={{ color: 'green' }}>{error}</p> : <p style={{ color: 'red' }}>{error}</p>
+      }
     </>
   )
 }
